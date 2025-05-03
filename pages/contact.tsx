f0084d2b5d5bb958366585df1179c1d9aabb4c0b
Mudassir Ghazi <amaan.khan848@gmail.com>
2025-05-03 16:51:47 +0530

    updated

---
+++ pages/contact.tsx
@@ -0,0 +1,108 @@
+// components/ContactSection.js
+'use client';
+import Link from "next/link";
+import { TiThMenu } from "react-icons/ti";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+export default function ContactSection() {
+
+     const [isOpen, setIsOpen] = useState(false);
+    
+      const handleClick = () => {
+        setIsOpen(!isOpen);
+      };
+
+  return (
+    <>
+    <nav className="fixed top-0 left-0 w-full z-50 bg-transparent backdrop-blur-md text-white shadow-sm">
+        <div className="relative max-w-7xl mx-auto px-5 py-3 flex justify-between md:justify-between items-center">
+          <h1 className="text-2xl  font-bold">Mudassir <span className="text-amber-700">Ghazi</span></h1>
+          <ul className="hidden lg:flex space-x-[100px]">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/programs">Programs</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div onClick={handleClick} className="lg:hidden">
+            {isOpen ? (
+              <IoClose className="text-4xl" />
+            ) : (
+              <TiThMenu className="text-4xl" />
+            )}
+          </div>
+        </div>
+        
+      </nav>
+
+      {
+        isOpen && (
+          <motion.div
+          initial={{ y: "-100%" }}
+          animate={{ y: isOpen ? "0%" : "-100%" }}
+          transition={{ duration: 0.3 }}
+          
+          className={`fixed top-0 left-0 w-full z-50  bg-zinc-900 overflow-hidden ease-in-out lg:hidden ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          }`}>
+            <ul className=" flex flex-col space-y-6 p-5 pt-8 items-center justify-center">
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/">Home</Link>
+              </li>
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/about">About</Link>
+              </li>
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/programs">Programs</Link>
+              </li>
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/contact">Contact</Link>
+              </li>
+            </ul>
+            <IoClose onClick={handleClick} className="absolute top-4 right-5 text-4xl " />
+          </motion.div>
+        )
+      }
+
+<div className="min-h-screen bg-zinc-900 px-4 py-12 pt-25">
+      <div className="max-w-3xl mx-auto bg-white shadow-md rounded-lg p-8">
+        <h1 className="text-3xl font-bold mb-6 text-center text-gray-700">Contact Me</h1>
+        
+        <div className="space-y-6 text-gray-700 text-lg">
+          <div>
+            <h2 className="font-semibold text-xl mb-1">Phone</h2>
+            <p>
+              <a href="tel:+918866672242" className="text-blue-600 hover:underline">
+                +918866672242
+              </a>
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold text-xl mb-1">Email</h2>
+            <p>
+              <a href="mailto:mudassirghazi1121@gmail.com" className="text-blue-600 hover:underline">
+                info@gmail.com
+              </a>
+            </p>
+          </div>
+
+          <div>
+            <h2 className="font-semibold text-xl mb-1">Home Address</h2>
+            <p>SunCity-3, Jamnagar, Gujrat, INDIA</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,147 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+import Contact from "./contact";
+import { TiThMenu } from "react-icons/ti";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+function HomePage() {
+ const [isOpen, setIsOpen] = useState(false);
+ const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-export default function Home() {
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <nav className="fixed top-0 left-0 w-full z-50 bg-transparent backdrop-blur-md text-white shadow-sm">
+        <div className="relative max-w-7xl mx-auto px-5 py-3 flex justify-between md:justify-between items-center">
+          <h1 className="text-2xl  font-bold">Mudassir <span className="text-amber-700">Ghazi</span></h1>
+          <ul className="hidden lg:flex space-x-[100px]">
+            <li className="hover:text-amber-700 hover:scale-110 transition-all duration-200">
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/programs">Programs</Link>
+            </li>
+           
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div onClick={handleClick} className="lg:hidden">
+            {isOpen ? (
+              <IoClose className="text-4xl" />
+            ) : (
+              <TiThMenu className="text-4xl" />
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        
+      </nav>
+
+      {
+        isOpen && (
+          <motion.div
+          initial={{ y: "-100%" }}
+          animate={{ y: isOpen ? "0%" : "-100%" }}
+          transition={{ duration: 0.3 }}
+          
+          className={`fixed top-0 left-0 w-full z-50  bg-zinc-900 overflow-hidden ease-in-out lg:hidden ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          }`}>
+            <ul className=" flex flex-col space-y-6 p-5 pt-8 items-center justify-center">
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/">Home</Link>
+              </li>
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/about">About</Link>
+              </li>
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/programs">Programs</Link>
+              </li>
+              
+              <li className="text-lg hover:text-amber-700 hover:scale-110 transition-all duration-200">
+                <Link onClick={handleClick} href="/contact">Contact</Link>
+              </li>
+            </ul>
+            <IoClose onClick={handleClick} className="absolute top-4 right-5 text-4xl " />
+          </motion.div>
+        )
+      }
+
+      
+
+      <section className="relative w-full h-screen">
+        {/* Background Image */}
+        <div
+          className="absolute inset-0 bg-cover bg-top bg-no-repeat"
+          style={{ backgroundImage: "url('/bgimg.jpg')" }}
+        />
+
+        {/* Dark Overlay */}
+        <div className="absolute inset-0 bg-black opacity-65 bg-gradient-to-l from-black to-gray-900" />
+
+        {/* Content */}
+        <div className="relative z-10 flex items-center justify-start h-full text-white text-center md:px-20 px-5">
+          <div className="text-left ">
+            <h1 className="text-5xl  md:text-7xl font-bold">
+              Train with <span className="text-amber-700 font-bold">ME.</span>
+            </h1>
+            <p className="mt-4 text-lg md:text-xl">
+              A Qualified and Experienced Personal Trainer
+            </p>
+            <a
+           
+           href="/resume.pdf"
+           download
+           className="inline-block px-3 py-1 bg-amber-700 text-white font-medium rounded-md shadow hover:bg-amber-600 transition duration-300 mt-14"
+         >
+           Resume
+         
+            </a>
+          </div>
+        </div>
+
+        <div className="w-full md:px-10 md:block lg:flex">
+          <div className="relative md:w-full lg:w-1/2 pt-[650px] ">
+            <img
+              className="absolute inset-0 w-[250px] md:w-[300px] top-10 left-15 md:top-10 md:left-15 object-cover rounded-2xl"
+              src="https://t4.ftcdn.net/jpg/01/74/21/25/360_F_174212531_cerVf4uP6vinBWieBB46p2P5xVhnsNSK.jpg"
+              alt=""
+            />
+            <img
+              className="absolute inset-0 w-[250px] md:w-[300px] top-45 left-50 md:top-50 md:left-80 object-cover rounded-2xl"
+              src="https://img.freepik.com/foto-gratis/mujer-atletica-haciendo-ejercicio-gimnasio-mantenerse-forma_23-2150989975.jpg?semt=ais_hybrid&w=740"
+              alt=""
+            />
+            <img
+              className="absolute inset-0 w-[250px] md:w-[300px] top-90 left-15 md:top-80 md:left-15 object-cover rounded-2xl"
+              src="/a2.jpg"
+              alt=""
+            />
+          </div>
+          <div className=" md:w-full lg:w-1/2  md:text-left px-10 ">
+            <div className="w-[80%] mx-auto jusctify-center items-center">
+              <h2 className="text-5xl font-bold mb-20 mt-[20px] md:mt-[50px] lg:mt-[260px]">
+                Transform your physique with our <span className="text-amber-700 font-bold">Fitness Plan.</span>
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full h-screen">
+        <Contact/>
+        </div>
+      </section>
+
+      
+    </>
   );
 }
+
+export default HomePage;
